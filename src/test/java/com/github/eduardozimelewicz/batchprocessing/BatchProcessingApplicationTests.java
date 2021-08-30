ca69ee60585419173b5ee564c55e3db981636a26
EduardoZimelewicz <eduardo.zimelewicz@gmail.com>
2021-08-30 19:14:42 -0300

    unit tests

--- src/test/java/com/github/eduardozimelewicz/batchprocessing/BatchProcessingApplicationTests.java
+++ src/test/java/com/github/eduardozimelewicz/batchprocessing/BatchProcessingApplicationTests.java
@@ -1,13 +1,54 @@
 package com.github.eduardozimelewicz.batchprocessing;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import com.github.eduardozimelewicz.batchprocessing.config.BatchConfig;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+@RunWith(SpringRunner.class)
+@SpringBatchTest
+@EnableAutoConfiguration
+@ContextConfiguration(classes = { BatchConfig.class })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+				DirtiesContextTestExecutionListener.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class BatchProcessingApplicationTests {
 
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
+	@Autowired
+	private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+	@After
+	public void cleanUp() {
+		jobRepositoryTestUtils.removeJobExecutions();
+	}
+
 	@Test
-	void contextLoads() {
+	public void batchExecutionCompletenessTest() throws Exception{
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+		JobInstance actualJobInstance = jobExecution.getJobInstance();
+		ExitStatus actualJobExitStatus = jobExecution.getExitStatus();
+
+		assertThat(actualJobInstance.getJobName()).isEqualTo("step1");
+		assertThat(actualJobExitStatus.getExitCode()).isEqualTo("COMPLETED");
 	}
 
 }
