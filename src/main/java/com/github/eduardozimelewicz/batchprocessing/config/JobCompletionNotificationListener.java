01e54726e089d1123fb1bf0f5a75a167a73d7ee6
EduardoZimelewicz <eduardo.zimelewicz@gmail.com>
2021-09-02 12:40:15 -0300

    children pipeline and refreshed code

--- src/main/java/com/github/eduardozimelewicz/batchprocessing/config/JobCompletionNotificationListener.java
+++ src/main/java/com/github/eduardozimelewicz/batchprocessing/config/JobCompletionNotificationListener.java
@@ -1,13 +1,10 @@
 package com.github.eduardozimelewicz.batchprocessing.config;
 
-import com.github.eduardozimelewicz.batchprocessing.entity.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,21 +12,10 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 
   private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-  //private final JdbcTemplate jdbcTemplate;
-
-  //@Autowired
-  //public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate;}
-
   @Override
   public void afterJob(JobExecution jobExecution) {
     if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
       log.info("!!! JOB FINISHED !!!");
-      
-      //jdbcTemplate.query("SELECT first_name, last_name FROM people",
-      //        (rs, row) -> new Person(
-      //                rs.getString(1),
-      //                rs.getString(2))
-      //).forEach(person -> log.info("Found <" + person + "> in the database."));
     } else {
       log.error("Job execution " + jobExecution.getId() + " failed");
     }
